791c9dad1ed05b5fd25b52980dcab831ec0d7316
aron <aronjames214@gmail.com>
2020-04-08 18:42:49 +0530

    first commit

--- sketch.js
+++ sketch.js
@@ -21,6 +21,7 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
+    box3 = new Box(500,400,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
